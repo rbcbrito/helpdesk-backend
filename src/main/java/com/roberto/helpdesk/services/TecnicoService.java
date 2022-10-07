a19e3725f1776d516ed520a9c5713cc83ada9638
Roberto Bruno <robertobcb@gmail.com>
2022-10-07 00:37:59 -0300

    Excecao para objeto não encontrado

--- src/main/java/com/roberto/helpdesk/services/TecnicoService.java
+++ src/main/java/com/roberto/helpdesk/services/TecnicoService.java
@@ -7,17 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.roberto.helpdesk.domain.Tecnico;
 import com.roberto.helpdesk.repositories.TecnicoRepository;
+import com.roberto.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
 
 	@Autowired
 	private TecnicoRepository repository;
-	
+
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
 	}
-	
-	
+
 }
