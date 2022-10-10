29f603ec36a7bcc75eb7e039d70b6c96ca256fa3
Roberto Bruno <robertobcb@gmail.com>
2022-10-10 19:00:22 -0300

    Endpoint create para Tecnico

--- src/main/java/com/roberto/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/roberto/helpdesk/domain/dtos/TecnicoDTO.java
@@ -25,6 +25,7 @@ public class TecnicoDTO implements Serializable {
 
 	public TecnicoDTO() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public TecnicoDTO(Tecnico obj) {
@@ -36,6 +37,7 @@ public class TecnicoDTO implements Serializable {
 		this.senha = obj.getSenha();
 		this.perfis = obj.getPerfis().stream().map(p -> p.getCodigo()).collect(Collectors.toSet());
 		this.dataCriação = obj.getDataCriação();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Integer getId() {
--- src/main/java/com/roberto/helpdesk/services/TecnicoService.java
+++ src/main/java/com/roberto/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.roberto.helpdesk.domain.Tecnico;
+import com.roberto.helpdesk.domain.dtos.TecnicoDTO;
 import com.roberto.helpdesk.repositories.TecnicoRepository;
 import com.roberto.helpdesk.services.exceptions.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class TecnicoService {
 	public List<Tecnico> findAll() {
 		return repository.findAll();
 	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
+	}
+
+	
 
 }
