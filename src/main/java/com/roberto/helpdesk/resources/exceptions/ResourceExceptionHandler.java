b5fd923ac7555d8eb787c6cf9194a566223ed73b
Roberto Bruno <robertobcb@gmail.com>
2022-10-10 19:27:36 -0300

    Exceção para CPF e Email já cadastrados

--- src/main/java/com/roberto/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/roberto/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -7,10 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.roberto.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.roberto.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
-public class ResourceExceptionHandler {
+public class ResourceExceptionHandler { 
 	
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex,
@@ -19,6 +20,15 @@ public class ResourceExceptionHandler {
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
 				"Object Not Found", ex.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationExceptionn(DataIntegrityViolationException ex,
+			HttpServletRequest request) { 
+		
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Violação de dados", ex.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 	
 }
