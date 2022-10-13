e44a85463496b986136a20dbd8cbf712fe92b0f8
Roberto Bruno <robertobcb@gmail.com>
2022-10-13 13:17:28 -0300

    Implementado update para Chamado

--- src/main/java/com/roberto/helpdesk/services/ChamadoService.java
+++ src/main/java/com/roberto/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.roberto.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -39,6 +40,13 @@ public class ChamadoService {
 	public Chamado create(@Valid ChamadoDTO objDTO) {
 		return repository.save(newChamado(objDTO));
 	}
+
+	public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+		objDTO.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamado(objDTO);
+		return repository.save(oldObj);
+	}
 	
 	private Chamado newChamado(ChamadoDTO obj) {
 		Tecnico tecnico = tecnicoService.findById(obj.getIdTecnico());
@@ -50,6 +58,10 @@ public class ChamadoService {
 		if(obj.getId() != null) {
 			chamado.setId(obj.getId());
 		}
+		
+		if(obj.getStatus().equals(2)) {
+			chamado.setDataFechamento(LocalDate.now());
+		}
 		chamado.setTecnico(tecnico);
 		chamado.setCliente(cliente);
 		chamado.setTitulo(obj.getTitulo());
@@ -59,6 +71,7 @@ public class ChamadoService {
 		return chamado;
 		
 	}
+
 	
 	
 	
